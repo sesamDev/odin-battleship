23ee802104306d5188994a33371f5ea975c9c13b
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-10-13 21:39:59 +0200

    Added tests to implement later

--- Tests/gameboard.test.js
+++ Tests/gameboard.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import Gameboard from "../Javascript/gameboard";
 
-describe("Gameboard tests", () => {
+describe("Placing ships", () => {
   test("Placing 1x1 ship", () => {
     const gameboard = new Gameboard();
     gameboard.placeShip(1, 1, 1);
@@ -16,4 +16,12 @@ describe("Gameboard tests", () => {
       [1, 3],
     ]);
   });
+
+  test.skip("Choosing direction and placing ship", () => {});
+
+  test.skip("Can't place outside of board", () => {});
+
+  test.skip("Can't place on used grid positions", () => {});
 });
+
+describe.skip("Handle attacks", () => {});
