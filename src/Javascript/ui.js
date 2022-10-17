d12586076397142d693e37c0a9e3ff783c0a522a
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-10-17 08:46:52 +0200

    Added element to display names of players

--- src/Javascript/ui.js
+++ src/Javascript/ui.js
@@ -8,17 +8,21 @@ export default class UI {
   static #appendToBody() {
     const container = document.createElement("div");
     container.classList.add("container");
-    const p1Board = this.#createGameboard();
-    const p2Board = this.#createGameboard();
+    const p1Board = this.#createGameboard("Sebastian");
+    const p2Board = this.#createGameboard("HAL");
 
     container.append(p1Board, p2Board);
     document.body.append(container);
   }
 
-  static #createGameboard() {
+  static #createGameboard(name) {
     const fragmnt = document.createDocumentFragment();
     const boardContainer = document.createElement("div");
+    const playerName = document.createElement("h2");
+    playerName.innerText = name;
     boardContainer.classList.add("board-container");
+
+    boardContainer.appendChild(playerName);
     for (let i = 0; i < this.gridXY[0]; i += 1) {
       for (let j = 0; j < this.gridXY[1]; j += 1) {
         const gridSquare = document.createElement("div");
@@ -42,5 +46,33 @@ export default class UI {
         });
       });
     });
+  }
+
+  // Not my best function..
+  static screenPositionToGridPosition(clickedPosition) {
+    let arr = [];
+    if (clickedPosition.length === 2) {
+      arr = clickedPosition.split("");
+    }
+    if (clickedPosition === "110") {
+      const a = clickedPosition.split("");
+      const b = a[1] + a[2];
+      arr.push(a[0]);
+      arr.push(b);
+    }
+    if (clickedPosition === "101") {
+      const a = clickedPosition.split("");
+      const b = a[0] + a[1];
+      arr.push(b);
+      arr.push(a[2]);
+    }
+    if (clickedPosition === "1010") {
+      const a = clickedPosition.split("");
+      const b = a[0] + a[1];
+      arr.push(b);
+      arr.push(a[2] + a[3]);
+    }
+    console.log(`From UI: ${arr} `);
+    return clickedPosition;
   }
 }
