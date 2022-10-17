b3e267d9a425744b58d1e0d66e80c013f58ec0fd
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-10-17 19:25:23 +0200

    Attacks can now only hit a position once

--- src/Javascript/computer.js
+++ src/Javascript/computer.js
@@ -4,12 +4,37 @@ export default class Computer extends Player {
   constructor(name) {
     super(name);
     this.turn = false;
+    this.availableMoves = (() => {
+      const arr = [];
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          arr.push([j + 1, i + 1]);
+        }
+      }
+      return arr;
+    })();
   }
 
   attack() {
-    const randX = Math.floor(Math.random() * 10) + 1; // Returns a random integer from 1 to 2:
-    const randY = Math.floor(Math.random() * 10) + 1; // Returns a random integer from 1 to 2:
+    const randAttackPos = this.#getRandomPosition();
+    console.log(`Attack pos: ${randAttackPos}`);
+    console.log(this.#removePosFromAvailableMoves(randAttackPos));
+    return [randAttackPos[0], randAttackPos[1]];
+  }
 
-    return [randX, randX];
+  // Return a random position [x,y], used for the attack func.
+  // Cannot return a given position more than once.
+  #getRandomPosition() {
+    const posArr = this.availableMoves;
+    const randIndex = Math.floor(Math.random() * posArr.length);
+
+    return posArr[randIndex];
+  }
+
+  // Used so #getRandomPosition wont get same position more than once.
+  #removePosFromAvailableMoves(pos) {
+    const arr = this.availableMoves;
+    const newArr = arr.filter((p) => p !== pos);
+    this.availableMoves = newArr;
   }
 }
